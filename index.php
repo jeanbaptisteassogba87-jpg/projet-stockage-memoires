0d3a71305150b42595caa65f95c25dab8d20d321
jeanbaptisteassogba87-jpg <jeanbaptisteassogba87@gmail.com>
2026-05-30 17:57:23 +0100

    fix: correction chemins de redirection

--- index.php
+++ index.php
@@ -1,45 +1,26 @@
 <?php
-
 require_once 'config/session.php';
 require_once 'config/constants.php';
 
-
 if (empty($_SESSION['user_id'])) {
-
-    header('Location: /projet-stockage-memoires/views/auth/login.php');
-
+    header('Location: /views/auth/login.php');
     exit;
 }
 
 switch ($_SESSION['user_role']) {
-
     case ROLE_ETUDIANT:
-
-        header('Location: /projet-stockage-memoires/views/etudiant/dashboard.php');
-
+        header('Location: /views/etudiant/dashboard.php');
         break;
-
     case ROLE_PROFESSEUR:
-
-        header('Location: /projet-stockage-memoires/views/professeur/dashboard.php');
-
+        header('Location: /views/professeur/dashboard.php');
         break;
-
     case ROLE_DIRECTEUR:
-
-        header('Location: /projet-stockage-memoires/views/directeur/dashboard.php');
-
+        header('Location: /views/directeur/dashboard.php');
         break;
-
     case ROLE_TECHNICIEN:
-
-        header('Location: /projet-stockage-memoires/views/technicien/dashboard.php');
-
+        header('Location: /views/technicien/dashboard.php');
         break;
-
     default:
-
-        header('Location: /projet-stockage-memoires/views/auth/login.php');
+        header('Location: /views/auth/login.php');
 }
-
 exit;
