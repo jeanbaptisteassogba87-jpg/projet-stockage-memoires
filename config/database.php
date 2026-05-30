eba53ea9bbf53f7af28f30d158d681d218bd60dd
jeanbaptisteassogba87-jpg <jeanbaptisteassogba87@gmail.com>
2026-05-30 12:33:37 +0100

    feat: connexion base de données

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $dbname = "gestion_memoires";
+    private $username = "root";
+    private $password = "";
+
+    public function connect() {
+        try {
+            $pdo = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname;charset=utf8",
+                $this->username,
+                $this->password
+            );
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch(PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+}
